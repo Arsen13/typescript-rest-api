2be805dde224ba867af85179a544eb5b1638cd27
Kohut Arsen <kohut.arsen@gmail.com>
2025-01-17 21:14:00 +0200

    Create user model and user interface

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+interface IUser extends mongoose.Document {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
+
+export default IUser;
+
+export interface BaseRepository<T> {
+    create(data: T): Promise<T>;
+    findAll(): Promise<T[]>;
+    findById(id: string): Promise<T | null>;
+    update(id: string, data: T): Promise<T | null>;
+    delete(id: string): Promise<T | null>;
+    findAllPaginatedWithFilter(
+        filter: any,
+        page: number,
+        limit: number
+    ): Promise<T[]>;
+}
